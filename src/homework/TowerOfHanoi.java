c6726950513ddac3403dcfdb12be73668b326b14
Wilson W <swat2129@bths.edu>
2016-02-06 21:17:53 -0500

    Finished solveTOH, my nonrecursive version to solve the Tower of Hanoi puzzle

--- src/homework/TowerOfHanoi.java
+++ src/homework/TowerOfHanoi.java
@@ -2,19 +2,39 @@ package homework;
 
 public class TowerOfHanoi {
 	
+	private static char[] pos = {'A', 'B', 'C'};
+	
 	// The number of disks: n
-	private static int n = 5;
+	// Poles from left to right is A, B, C
+	// Top disk is 0 and bottom disk is n - 1
+	
+	private static int n = 3;
+	private static int startPos = 0;
+	private static int[] disksPos= new int[5];
 	
 	public static void main(String[] args) {		
+		for (int i = 0; i < n; i++)
+			disksPos[i] = startPos;
+		
 		solveTOH();
 	}
 	
 	public static void solveTOH() {
 		for (int i = 1; i < (int)Math.pow(2, n); i++) {
 			System.out.printf("Step %d - ", i);
+			
 			for (int j = n - 1; j > -1; j--) {
 				if (i % (int)Math.pow(2, j) == 0) {
-					System.out.printf("Move Disk %d\n", j);
+					int shift = j % 2 == 0 ? 1 : -1;
+					
+					System.out.printf("Move Disk %d from %C to ", j, pos[disksPos[j]]);
+					
+					disksPos[j] += shift;
+					if (disksPos[j] < 0) disksPos[j] += 3;
+					else disksPos[j] %= 3;
+					
+					System.out.printf("%C.\n", pos[disksPos[j]]);
+					
 					break;
 				}
 			}
