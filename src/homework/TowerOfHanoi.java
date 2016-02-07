d952a37b84de64451d388f6dcd53cd212a204324
Wilson W <swat2129@bths.edu>
2016-02-06 20:53:36 -0500

    Updated solveTOH with the correct disk movements

--- src/homework/TowerOfHanoi.java
+++ src/homework/TowerOfHanoi.java
@@ -3,13 +3,21 @@ package homework;
 public class TowerOfHanoi {
 	
 	// The number of disks: n
-	private static int n = 2;
+	private static int n = 5;
 	
 	public static void main(String[] args) {		
 		solveTOH();
 	}
 	
 	public static void solveTOH() {
-
+		for (int i = 1; i < (int)Math.pow(2, n); i++) {
+			System.out.printf("Step %d - ", i);
+			for (int j = n - 1; j > -1; j--) {
+				if (i % (int)Math.pow(2, j) == 0) {
+					System.out.printf("Move Disk %d\n", j);
+					break;
+				}
+			}
+		}
 	}
 }
