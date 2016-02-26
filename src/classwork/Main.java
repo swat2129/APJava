c8d1a38a2b7a00ca2e9b7f6cfeb9e576e57900f0
swat2129 <swat2129@bths.edu>
2016-02-26 13:40:19 -0500

    Removed 2 lines

--- src/classwork/Main.java
+++ src/classwork/Main.java
@@ -4,17 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		HiddenWord puzzle = new HiddenWord("HARPS");
 		
-		System.out.println("Version 1:");
 		long startTime = System.nanoTime();
-		System.out.println(puzzle.getHint("HEART"));
+		System.out.println("Version 1: \n" + puzzle.getHint("HEART"));
 		long endTime = System.nanoTime();
 		System.out.printf("Nanoseconds taken: %d\n", endTime - startTime);
 		
 		System.out.println();
 		
-		System.out.println("Version 2:");
 		startTime = System.nanoTime();
-		System.out.println(puzzle.getHintv2("HEART"));
+		System.out.println("Version 2: \n" + puzzle.getHintv2("HEART"));
 		endTime = System.nanoTime();
 		System.out.printf("Nanoseconds taken: %d\n", endTime - startTime);
 	}
