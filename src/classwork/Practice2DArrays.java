eddca17d95f50e8d4c4500da035833ec87fa5555
Wilson Wat <wilson@new-host-3.home>
2016-03-01 20:09:29 -0500

    Added something

--- src/classwork/Practice2DArrays.java
+++ src/classwork/Practice2DArrays.java
@@ -1,5 +1,11 @@
 package classwork;
 
 public class Practice2DArrays {
-
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int[][] posToNeg2D(int[][] mat, int x, int y) {
+		return null;
+	}
 }
