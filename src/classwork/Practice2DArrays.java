89aaacff523bda47c2bc2de66a8c89045c655c46
swat2129 <swat2129@bths.edu>
2016-03-10 12:40:18 -0500

    Update something

--- src/classwork/Practice2DArrays.java
+++ src/classwork/Practice2DArrays.java
@@ -3,19 +3,36 @@ package classwork;
 public class Practice2DArrays {
 	public static void main(String[] args) {
 		int[][] testArr = {{9, 8, 7, 6}, {5, 4, 2, 1}, {3, 9, 2, 3}};
-		// posToNeg2D(testArr, 0, 0);
 		printDoubleArr(posToNeg2D(testArr, 1, 1));
+		
 		System.out.println();
+		
+		int[][] testArr2 = {{9, 8, 7, 6}, {5, 4, 2, 1}, {3, 9, 2, 3}};
+		printDoubleArr(posToNeg2Dv2(testArr2, 1, 1));
+		
+		System.out.println();
+		
 		printArr(rowMajorOrder(testArr));
 	}
 	
 	public static int[][] posToNeg2D(int[][] mat, int x, int y) {
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
-				if (i == x && j == y) {
-					
-				} else if (Math.abs(x - i) < 2 && Math.abs(y - j) < 2) {
+				if ((Math.abs(x - i) < 2 && Math.abs(y - j) < 2) && (i != x || j != y)) {
 					mat[i][j] = Math.abs(mat[i][j]) * -1;
+				}
+			}
+		}
+		return mat;
+	}
+	
+	public static int[][] posToNeg2Dv2(int[][] mat, int x, int y) {
+		for (int i = x - 1; i < x + 2; i++) {
+			if (i > -1 && i < mat.length) {
+				for (int j = y - 1; j < y + 2; j++) {
+					if (j > -1 && j < mat[i].length && (i != x || j != y)) {
+						mat[i][j] = Math.abs(mat[i][j]) * -1;
+					}
 				}
 			}
 		}
---
+++ src/homework/Codeboard.java
@@ -0,0 +1,95 @@
+package homework;
+
+public class Codeboard {
+	
+    public int maxDistBetweenAny2(int[][] nums){
+        int min = nums[0][0];
+        int max = nums[0][0];
+        
+        for (int[] subArr: nums) {
+            for (int i: subArr) {
+                if (i < min) min = i;
+                if (i > max) max = i;
+            }
+        }
+        
+        return max - min;
+    }
+	
+    public String[][] twoCharsTo2D(String[][] table, String[] words){
+        int count = 0;
+        
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                table[i][j] = count < words.length ? words[count].substring(0, 2) : "$$";
+                count++;
+            }
+        }
+        
+        return table;
+    }
+	
+    public String bestAverage(String[] roster, int[][] grades){
+        int person = 0;
+        double currentMax = Double.MIN_VALUE;
+        
+        for (int i = 0; i < grades.length; i++) {
+            int sum = 0;
+            
+            for (int num: grades[i]) {
+                sum += num;
+            }
+            
+            double avg = sum / grades[i].length;
+            
+            if (avg > currentMax) {
+                currentMax = avg;
+                person = i;
+            }
+        }
+        
+        return roster[person];
+    }
+	
+    public int[][] removeRowCol(int[][] mat, int row, int col){
+        int[][] nothing = new int[mat.length - 1][mat[0].length - 1];
+        
+        for (int i = 0; i < nothing.length; i++) {
+            for (int j = 0; j < nothing[0].length; j++) {
+                if (i < row) nothing[i][j] = j < col ? mat[i][j] : mat[i][j + 1];
+                else nothing[i][j] = j < col ? mat[i + 1][j] : mat[i + 1][j + 1];
+            }
+        }
+        
+        return nothing;
+    }
+	
+    public int[] getNeighbors(int[][] grid, int row, int col) {
+        int[] r;
+        
+        if (row == 0 || row == grid.length - 1) {
+            if (col == 0 || col == grid[0].length - 1) {
+                r = new int[3];
+            } else {
+                r = new int[5];
+            }
+        } else {
+            if (col == 0 || col == grid[0].length - 1) {
+                r = new int[5];
+            } else {
+                r = new int[8];
+            }
+        }
+        
+        int count = 0;
+        for (int i = row - 1; i < row + 2; i++) {
+            for (int j = col - 1; j < col + 2; j++) {
+                if (i > -1 && i < grid.length && j > -1 && j < grid[0].length) {
+                    r[count] = grid[i][j];
+                }
+            }
+        }
+        
+        return r;
+    }
+}
