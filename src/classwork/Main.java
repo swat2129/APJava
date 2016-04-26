6ce16336bb997ab1e33346f0cb7f469d2c744f85
Wilson W <swat2129@bths.edu>
2016-04-26 19:01:37 -0400

    I don't know

--- src/classwork/Main.java
+++ src/classwork/Main.java
@@ -2,6 +2,7 @@ package classwork;
 
 public class Main {
 	public static void main(String[] args) {
+		/*
 		HiddenWord puzzle = new HiddenWord("HARPS");
 		
 		long startTime = System.nanoTime();
@@ -15,6 +16,22 @@ public class Main {
 		System.out.println("Version 2: \n" + puzzle.getHintv2("HEART"));
 		endTime = System.nanoTime();
 		System.out.printf("Nanoseconds taken: %d\n", endTime - startTime);
-
+		*/
+		
+		System.out.println(isCompleteSubset("adlfjkalsdf", "ad"));
+	}
+	
+	public static boolean isCompleteSubset(String group, String other) {
+		String temp = other;
+		for (int i = 0; i < group.length(); i++) {
+			String letter = group.substring(i, i+1);
+			if (temp.indexOf(letter) < 0) return false;
+			else temp = temp.substring(0,temp.indexOf(letter)) + '-' + temp.substring(temp.indexOf(letter) + 1);
+		}
+		return true;
+	}
+	
+	public static boolean contains(String group, String other) {
+		return group.indexOf(other) >= 0;
 	}
 }
